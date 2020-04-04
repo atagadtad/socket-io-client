8a5b44de351522ab61657df772be3ab1db2d34dd
Aaron Tagadtad <atagadtad@creativeguild.ca>
2020-04-04 00:21:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import socketIOClient from "socket.io-client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const socket = socketIOClient("http://localhost:3001");
+    socket.on();
+  }, []);
+  return <div className="App">hai</div>;
 }
 
 export default App;
