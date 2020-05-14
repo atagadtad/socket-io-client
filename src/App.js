df671329f142af3f2d0eb9f691c85ff92256aebf
Aaron Tagadtad <atagadtad@creativeguild.ca>
2020-05-14 16:45:25 -0400

    added stateContext.js for useContext hook and finished useEffect function for receving socket.io data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,79 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import "./App.css";
 import io from "socket.io-client";
 import Peer from "simple-peer";
+import StateContext from "./StateContext";
 
 function App() {
+  const [yourID, setYourID] = useState("");
+  const [users, setUsers] = useState({});
+  const [stream, setStream] = useState(null);
+  const [callAccepted, setCallAccepted] = useState(false);
+
+  const initialStates = {
+    receivingCall: false,
+    caller: "",
+    callerSignal: null,
+  };
+
+  const stateReducer = (state, action) => {
+    switch (action.type) {
+      case "SOMEONE_IS_CALLING_YOU":
+        return {
+          ...state,
+          receivingCall: true,
+          caller: action.data.from,
+          callerSignal: action.data.signal,
+        };
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type: ${action.type}`
+        );
+    }
+  };
+
+  const [currentStates, dispatchCurrentStates] = useReducer(
+    stateReducer,
+    initialStates
+  );
+
+  const receivingCall = currentStates.receivingCall;
+  const caller = currentStates.caller;
+  const callerSignal = currentStates.callerSignal;
+
+  const handleReceivingCall = (data) => {
+    dispatchCurrentStates({ type: "SOMEONE_IS_CALLING_YOU", data });
+  };
+
+  const userVideo = useRef();
+  const partnerVideo = useRef();
+  const socket = useRef();
+
   useEffect(() => {
-    const socket = socketIOClient("http://localhost:3001");
-    socket.on("notifications_1", (msg) => {
-      console.log({ msg });
+    socket.current = io.connect("/");
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        setStream(stream);
+        if (userVideo.current) {
+          userVideo.current.srcObject = stream;
+        }
+      });
+
+    socket.current.on("yourID", (id) => {
+      setYourID(id);
+    });
+    socket.current.on("allUsers", (users) => {
+      setUsers(users);
+    });
+
+    socket.current.on("hey", (data) => {
+      handleReceivingCall(data);
     });
   }, []);
-
-  useEffect(() => {});
 
   return (
     <div className="App">
