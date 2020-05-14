e0fcc0940445ef03bc2b2dcbd05b3e0ac14d906f
Aaron Tagadtad <atagadtad@creativeguild.ca>
2020-05-14 17:06:16 -0400

    added receive and accept Call functions in app.js

--- src/App.js
+++ src/App.js
@@ -75,6 +75,56 @@ function App() {
     });
   }, []);
 
+  const callPeer = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", (data) => {
+      socket.current.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: yourID,
+      });
+    });
+
+    peer.on("stream", (stream) => {
+      if (partnerVideo.current) {
+        partnerVideo.current.srcObject = stream;
+      }
+    });
+
+    socket.current.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+  };
+
+  const acceptCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({
+      initiator: false,
+      tricle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", (data) => {
+      socket.current.emit("acceptCall", {
+        signal: data,
+        to: caller,
+      });
+    });
+
+    peer.on("stream", (stream) => {
+      partnerVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+  };
+
   return (
     <div className="App">
       <h1>hello world</h1>
